16a60fb03c969b0110932a46e5fe225fd02d275d
André Schmidt Ribeiro <andresr94@hotmail.com>
2022-02-16 15:10:03 -0300

    Cliente, TipoCliente, Telefone e Enderecos adicionados

--- src/main/java/com/bilboSoftware/cursomc/CursomcApplication.java
+++ src/main/java/com/bilboSoftware/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.bilboSoftware.cursomc.domain.Categoria;
 import com.bilboSoftware.cursomc.domain.Cidade;
+import com.bilboSoftware.cursomc.domain.Cliente;
+import com.bilboSoftware.cursomc.domain.Endereco;
 import com.bilboSoftware.cursomc.domain.Estado;
 import com.bilboSoftware.cursomc.domain.Produto;
+import com.bilboSoftware.cursomc.domain.enums.TipoCliente;
 import com.bilboSoftware.cursomc.repositories.CategoriaRepository;
 import com.bilboSoftware.cursomc.repositories.CidadeRepository;
+import com.bilboSoftware.cursomc.repositories.ClienteRepository;
+import com.bilboSoftware.cursomc.repositories.EnderecoRepository;
 import com.bilboSoftware.cursomc.repositories.EstadoRepository;
 import com.bilboSoftware.cursomc.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -52,6 +63,14 @@ public class CursomcApplication implements CommandLineRunner{
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
 		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apt 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 		
@@ -66,7 +85,9 @@ public class CursomcApplication implements CommandLineRunner{
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
-
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 	}
 
 }
