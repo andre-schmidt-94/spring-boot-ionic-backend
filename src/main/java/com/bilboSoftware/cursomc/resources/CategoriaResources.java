f55df5019567878ec820a4cdbf3835d64c09d366
André Schmidt Ribeiro <andresr94@hotmail.com>
2022-02-16 12:12:24 -0300

    Criado repository e service para Categoria

--- src/main/java/com/bilboSoftware/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/bilboSoftware/cursomc/resources/CategoriaResources.java
@@ -3,26 +3,29 @@ package com.bilboSoftware.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bilboSoftware.cursomc.domain.Categoria;
+import com.bilboSoftware.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="{Id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer Id) {
 		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
+		Categoria obj = service.buscar(Id);
 		
 		
-		return lista;
+		return ResponseEntity.ok().body(obj);
 	}
 }
