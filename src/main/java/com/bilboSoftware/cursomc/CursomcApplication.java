081262e3727439897b8ed1bdda3a467c6b422939
André Schmidt Ribeiro <andresr94@hotmail.com>
2022-02-16 12:23:06 -0300

    Criado e testado operacao de instanciacao

--- src/main/java/com/bilboSoftware/cursomc/CursomcApplication.java
+++ src/main/java/com/bilboSoftware/cursomc/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.bilboSoftware.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.bilboSoftware.cursomc.domain.Categoria;
+import com.bilboSoftware.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
