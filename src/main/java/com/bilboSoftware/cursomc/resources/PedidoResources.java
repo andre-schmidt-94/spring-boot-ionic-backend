97595a8e182a19581325dd6b78a5e60892aca3d2
André Schmidt Ribeiro <andresr94@hotmail.com>
2022-02-17 17:16:35 -0300

    Endpoint /pedidos/{id} adicionado/testado

---
+++ src/main/java/com/bilboSoftware/cursomc/resources/PedidoResources.java
@@ -0,0 +1,27 @@
+package com.bilboSoftware.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bilboSoftware.cursomc.domain.Pedido;
+import com.bilboSoftware.cursomc.services.PedidoService;
+
+@RestController
+@RequestMapping(value = "/pedidos")
+public class PedidoResources {
+
+	@Autowired
+	private PedidoService service;
+
+	@RequestMapping(value = "{Id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer Id) {
+
+		Pedido obj = service.buscar(Id);
+
+		return ResponseEntity.ok().body(obj);
+	}
+}
---
+++ src/main/java/com/bilboSoftware/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.bilboSoftware.cursomc.services;
+
+import java.util.Optional;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bilboSoftware.cursomc.domain.Pedido;
+import com.bilboSoftware.cursomc.repositories.PedidoRepository;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName(), null));
+	}
+}
